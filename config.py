2790ed7cb149186bb172a4d6dec963bd77f5b459
ihciah <ihciah@gmail.com>
2017-09-23 02:56:24 +0800

    Add comments.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# Your telegram token (Create one if you haven't got it: https://telegram.me/botfather)
 TG_TOKEN = "YOUR_TELEGRAM_TOKEN"
 CHAT_ID = [259444514, 259444514]  # Your chat id to yourself (2 items for dual sim card)
 
@@ -7,11 +8,11 @@ KB_COMMAND = ["kb", u"课表"]
 KB_IMAGE = "https://unknown"
 KB_IMAGE_CAPTION = "小海的课表"
 
-FIND_CONTACT_COMMAND = ["search", "contact", "num"]  # Command to query contact book
+FIND_CONTACT_COMMAND = ["search", "contact", "num"]  # Commands to query contact book
 
-SMS_COMMAND = ["sms"]  # Command to send sms
+SMS_COMMAND = ["sms"]  # Commands to send sms
 
-# Proxy used when connect telegram
+# Proxy to connect to telegram
 USE_PROXY = True
 _PROXY = dict(http='socks5://127.0.0.1:16801', https='socks5://127.0.0.1:16801')
 PROXY = None if not USE_PROXY else _PROXY
